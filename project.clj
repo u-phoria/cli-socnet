ab9bcd95363f8e60adb34fef0152b14e542e0005
Uros Rapajic <uros.rapajic@gmail.com>
2014-03-02 22:36:25 +0000

    Adding :aot :all to uberjar profile to suppress warning

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
                  [com.ocpsoft/ocpsoft-pretty-time "1.0.6"]]
-  :profiles {:dev {:dependencies [[speclj "2.6.1"]]}}
+  :profiles {:dev {:dependencies [[speclj "2.6.1"]]}
+             :uberjar {:aot :all}}
   :plugins [[speclj "2.6.1"]]
   :test-paths ["spec"]
   :main cli-socnet.cli)
